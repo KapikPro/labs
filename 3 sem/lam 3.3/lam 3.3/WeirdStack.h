cfcd6ce040f1f9368cd237af993321a14ff13b26
Yaroslav Senchin <yrik200315@gmail.com>
2022-10-04 21:51:41 +0300

    1.2

---
+++ 3 sem/lam 3.3/lam 3.3/WeirdStack.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include <exception>
+using namespace std;
+
+class WeirdStack
+{
+public:
+	WeirdStack()
+	{
+		_size = 0;
+		_capacity = 1;
+
+		_data = (int*)malloc(_capacity * sizeof(int));
+	}
+
+	~WeirdStack()
+	{
+		free((int*)_data);
+	}
+
+	int get_size()
+	{
+		return _size;
+	}
+
+	void operator++ ()
+	{
+		++_size;
+		int one = 1;
+		if (_size == _capacity)
+		{
+			_capacity *= 2;
+			_data = (int*)realloc(_data, _capacity * sizeof(int));
+		}
+
+		_data[_size - 1] = one;
+
+	}
+
+	void operator-- ()
+	{
+		if (_size == 0)
+			throw exception("ошибка, обьем равен 0");
+
+		--_size;
+
+	}
+
+	int get_top()
+	{
+		if (_size == 0)
+			throw exception("ошибка");
+
+		return _data[_size - 1];
+	}
+
+private:
+	int _size;
+	int _capacity;
+	int* _data;
+};
---
+++ 3 sem/lam 3.3/lam 3.3/tools.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include<iostream>
+#include<sstream>
+#include<string>
+using namespace std;
+
+long str_to_long(string h)
+{
+	stringstream a;
+	long ch;
+	a << h;
+	a >> ch;
+	return ch;
+}
+
+bool is_int(string str)
+{
+	for (auto c : str)
+		if (c < '0' || c > '9')
+			return false;
+	return true;
+}
+bool try_read_int(long &val)
+{
+	string s;
+	cin >> s;
+
+	if (s.size() > 18 || !is_int(s))
+		return 0;
+
+	val = str_to_long(s);
+	return 1;
+}
