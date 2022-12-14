adb0abbfa53d2c01bfcb7bdd473d4c09391f3324
Yaroslav Senchin <yrik200315@gmail.com>
2022-12-14 04:06:42 +0300

    lab 8

---
+++ 3 sem/lab 8/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_8
+{
+    delegate void OoO();
+
+    interface Info
+    {
+        void Viv_Info();
+    }
+    interface Sost
+    {
+        void Viv_Sost();
+    }
+
+    abstract class Person : Info
+    {
+        public Person(string f, string i, string o)
+        {
+            _f = f;
+            _i = i;
+            _o = o;
+        }
+        public string get_f()
+        {
+            return _f;
+        }
+        public string get_i()
+        {
+            return _i;
+        }
+        public string get_o()
+        {
+            return _o;
+        }
+        public abstract void Viv_Info();
+        private
+        string _f;
+        string _i;
+        string _o;
+    }
+
+    class Student : Person, Sost
+    {
+        public Student(string f, string i, string o, string har) : base(f, i, o)
+        {
+            _har = har;
+        }
+        public override void Viv_Info()
+        {
+            Console.WriteLine("Вывод информации о студенте:");
+            Console.WriteLine($"ФИО - {get_f()} { get_i()} { get_o()}");
+        }
+        public void Viv_Sost()
+        {
+            Console.WriteLine($"Вывод характеристики студента - { _har}");
+        }
+        private
+
+            string _har;
+    }
+
+    class old : Person
+    {
+        public old(string f, string i, string o, float zarp) : base(f, i, o)
+        {
+            _zarp = zarp;
+        }
+        public float get_zarp()
+        {
+            return _zarp;
+        }
+        public override void Viv_Info()
+        {
+
+        }
+        private
+          float _zarp;
+
+    }
+    class Prep : old
+    {
+        public Prep(string f, string i, string o, float zarp, int stazh) : base(f, i, o, zarp)
+        {
+            _stazh = stazh;
+        }
+        public override void Viv_Info()
+        {
+            Console.WriteLine("Вывод информации о преподавателе:");
+            Console.WriteLine($"ФИО - { get_f()} { get_i()} { get_o()}");
+            Console.WriteLine($"Его зарплата = { get_zarp()} рублей");
+            Console.WriteLine($"Его стаж составляет { _stazh } лет");
+        }
+
+        private
+          int _stazh;
+    }
+
+    class Zav : old
+    {
+        public Zav(string f, string i, string o, float zarp,string kaf) : base(f, i, o, zarp)
+        {
+            _kaf = kaf; 
+        }
+        public override void Viv_Info()
+        {
+            Console.WriteLine("Вывод информации о заведующем кафедрой:");
+            Console.WriteLine($"ФИО - { get_f()} { get_i()} { get_o()}");
+            Console.WriteLine($"Его зарплата = { get_zarp()}");
+            Console.WriteLine($"Он заведует кафедрой { _kaf}") ;
+        }
+
+        private
+        string _kaf;
+    }
+}
---
+++ 3 sem/lab 8/Class2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace lab_8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OoO? methods=null;
+            string str,f,i,o,kaf,har;
+            int uch=0, pr=0, zv=0;
+            int a, b, k = 1, stazh=0;
+            float zarp = 0;
+            while (true)
+            {
+                Console.WriteLine("\nменю:");
+                Console.WriteLine("1-добавить/переопределить ученика\n2-добавить/переопределить преподавателя\n3-добавить/переопределить заведущего кафедрой\n4-вывести всех добавленный вместе с их данными\n5-перезапустить программу\n");
+                //(вывод производится с помощью многоадресного делегата, т.е. он сразу проверяется)
+                while ((!int.TryParse(Console.ReadLine(), out a)) || (a != 1 && a != 2 && a != 3 && a != 4 && a != 5))
+                    Console.WriteLine("вы ввели что-то страшное, повторите ввод\n");
+                if (k == 0)
+                    break;
+                switch (a)
+                {
+                    case (1):
+                   
+                            Console.WriteLine("введите фамилию ученика");
+                            f = Console.ReadLine();
+                            while (!(new Regex("[а-я]+").IsMatch(f)))
+                            {
+                                Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                                f = Console.ReadLine();
+                            }
+                            Console.WriteLine("введите имя ученика");
+                            i = Console.ReadLine();
+                            while (!(new Regex("[а-я]+").IsMatch(i)))
+                            {
+                                Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                                i = Console.ReadLine();
+                            }
+                            Console.WriteLine("введите отчество ученика");
+                            o = Console.ReadLine();
+                            while (!(new Regex("[а-я]+").IsMatch(o)))
+                            {
+                                Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                                o = Console.ReadLine();
+                            }
+                            Console.WriteLine("введите характеристику ученика ученика(отличник, хорошист и т.п.)");
+                            har = Console.ReadLine();
+                            while (!(new Regex("[а-я]+").IsMatch(har)))
+                            {
+                                Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                                har = Console.ReadLine();
+                            }
+                            Student Alex = new Student(f, i, o,  har);  
+                            methods += Alex.Viv_Info;
+                            methods += Alex.Viv_Sost;
+
+                        break;
+                    case (2):
+                        Console.WriteLine("введите фамилию преподавателя");
+                        f = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(f)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            f = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите имя преподавателя");
+                        i = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(i)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            i = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите отчество преподавателя");
+                        o = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(o)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            o = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите зарплату преподавателя");
+                        while (!float.TryParse(Console.ReadLine(), out zarp))
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                        Console.WriteLine("введите стаж преподавателя(целых лет)");
+                        while (!int.TryParse(Console.ReadLine(), out stazh))
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                        Prep Bob = new Prep( f,  i,  o,  zarp,  stazh);
+                        methods += Bob.Viv_Info;
+                        break;
+                    case (3):
+                        Console.WriteLine("введите фамилию заведующего кафедры");
+                        f = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(f)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            f = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите имя заведующего кафедры");
+                        i = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(i)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            i = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите отчество заведующего кафедры");
+                        o = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(o)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            o = Console.ReadLine();
+                        }
+                        Console.WriteLine("введите зарплату заведующего кафедры");
+                        while (!float.TryParse(Console.ReadLine(), out zarp))
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                        Console.WriteLine("введите кафедру заведующего");
+                        kaf = Console.ReadLine();
+                        while (!(new Regex("[а-я]+").IsMatch(kaf)))
+                        {
+                            Console.WriteLine("вы ввели что-то страшное(или на английском), повторите ввод");
+                            kaf = Console.ReadLine();
+                        }
+                        Zav Igor = new Zav(f, i, o, zarp, kaf);
+                        methods += Igor.Viv_Info;
+                        break;
+                    case (4):
+                        if (methods != null)
+                            methods.Invoke();
+                        else
+                            Console.WriteLine("студент, преподаватель и зав кафедры не созданы");
+                        break;
+                    case (5):
+                        Main(args);
+                        break;
+                }
+            
+            }
+        }
+    }
+}
+
+
