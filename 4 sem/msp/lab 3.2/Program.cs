64e4d6b31fcdd70d4b5f7cbc17e51449ebb979b7
Yaroslav Senchin <yrik200315@gmail.com>
2023-03-16 22:30:51 +0300

    lab 3

---
+++ 4 sem/msp/lab 3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку для кодирования:\n");
+            string s = Console.ReadLine();
+            Console.Write("Введите ключ:\n");
+            int key = Convert.ToInt32(Console.ReadLine(), 2);
+            int izm = (Convert.ToInt32(s, 2) ^ key);
+            Console.WriteLine("Закодированный код: {0}", Convert.ToString(izm, 2));
+            izm = Convert.ToInt32(s, 2) ^ key;
+            Console.WriteLine("Декодированный код: {0}", Convert.ToString(izm, 2));
+            Console.ReadKey();
+        }
+    }
+}
